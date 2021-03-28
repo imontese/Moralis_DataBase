8c4db8de915bf207f7c681e76090cca61acd6ded
imon <imontese@gmail.com>
2021-03-28 17:18:25 +0200

    add solidity contract

--- Flip Coin/main.js
+++ Flip Coin/main.js
@@ -1,15 +1,30 @@
   
-Moralis.initialize("INSERT_APP_ID"); // Application id from moralis.io
-Moralis.serverURL = "INSERT_SERVER_URL"; //Server url from moralis.io
+Moralis.initialize("Zz4AXvfNWsSmCRb04NdUnRewYVKYUcTWfq1KVKmm"); // Application id from moralis.io
+Moralis.serverURL = "https://plrp70s8jxv4.moralis.io:2053/server"; //Server url from moralis.io
 
 async function login() {
     try {
+        user = await Moralis.User.current();
+        if(!user){
+            user = await Moralis.Web3.authenticate();
+        }
+
         user = await Moralis.Web3.authenticate();
         console.log(user);
         alert("User logged in")
+        document.getElementById("login_button").style.display = "none";
+        document.getElementById("game").style.display = "block";
     } catch (error) {
         console.log(error);
     }
 }
 
+async function flip(side){
+    let amount = document.getElementById("amount").value;
+     alert(side + '  ' + amount)
+
+}
+
 document.getElementById("login_button").onclick = login;
+document.getElementById("heads_button").onclick = function(){flip("heads")};
+document.getElementById("tails_button").onclick = function(){flip("tails")};
